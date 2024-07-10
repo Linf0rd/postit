ed105c9aec6a8a125b94bb8a3aa5144fe2c77f76
Linf0rd <linfordsnagger@gmx.com>
2024-07-10 07:12:02 +0200

    Wrapped auth and root routes with AuthLayout and RootLayout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 import SignInForm from './_auth/forms/SignInForm';
 import SignUpForm from './_auth/forms/SignUpForm';
 import { Home } from './_root/pages';
@@ -9,11 +11,15 @@ const App = () => {
     <main className='flex h-screen'>
       <Routes>
         {/* public routes */}
-        <Route path='/sign-in' element={<SignInForm />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SignInForm />} />
+          <Route path='/sign-up' element={<SignUpForm />} />
+        </Route>
 
         {/* private routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
