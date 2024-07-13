cff678c916192065c12759a50c7a1d12d27e8c9c
Linf0rd <linfordsnagger@gmx.com>
2024-07-13 17:50:59 +0200

    Add user account creation and saving to database
    Introducing two new functions, [createUserAccount] and [saveUserToDB], to handle the creation of a new user account and saving the user data to the database.
    
    [createUserAccount] takes in a `user` object with properties like `name`, `email`, `password`, and `username`, and creates a new account using the `account.create` method from the `appwrite` library. It also generates an avatar URL and saves the user data to the database using the [saveUserToDB] function.
    
    [saveUserToDB] takes in a `user` object with properties like `accountId`, `email`, `name`, `imageUrl`, and `username`, and saves the user data to the database using the `databases.createDocument` method from the `appwrite` library.
    
    These functions are designed to be used together to handle the complete process of creating a new user account and saving the user data to the database.

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,7 @@
 import { ID } from "appwrite";
 
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appWriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -12,9 +12,42 @@ export async function createUserAccount(user: INewUser) {
       user.name
     )
 
-    return newAccount;
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: new URL(avatarUrl),
+    })
+
+    return newUser;
   } catch (error) {
     console.log(error);
     return error;
+  }
+}
+
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appWriteConfig.databaseId,
+      appWriteConfig.userCollectionId,
+      ID.unique(),
+      user,
+    )
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
   }
 }
